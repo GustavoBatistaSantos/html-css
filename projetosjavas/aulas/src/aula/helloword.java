547786465b76d82b48a7ba36f1302494bdc3cec2
gustavo1506 <145714578+gustavo1506@users.noreply.github.com>
2025-02-20 12:50:53 -0300

    cx

--- projetosjavas/aulas/src/aula/helloword.java
+++ projetosjavas/aulas/src/aula/helloword.java
@@ -1,24 +1,25 @@
 package aula;
 
 public class helloword {
-    public static void main(String[] args) {
+    public static void main(String[] args) {//método construtor para rodar o programa
 
         
 
 
-        String nome = "Gustavo Batista santos";
-        byte idade = 65; //-128 ate +128
+        String nome = "Gustavo Batista santos"; //variavel para um texto 
+        byte idade = 65; //-128 ate +128  variavel para numeros até 128 e -128
         int numeroDeRg = 45698;
         double cpf = 18.50;
         float cpf2 = 50.50f;
         boolean sexo = true; 
 
-        System.out.println(nome);
-        System.out.println(idade);
+        System.out.println(nome); //metodo para escrever nome na tela
+        System.out.println(idade); // metodo para escrever a idade na tela
         System.out.println(numeroDeRg);
         System.out.println(cpf);
         System.out.println(cpf2);
         System.out.println(sexo);
+
     }
     
 }
