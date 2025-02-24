d57b84981f92a92fe8e7133f9661b646280c19b9
gustavo1506 <145714578+gustavo1506@users.noreply.github.com>
2025-02-24 12:19:09 -0300

    ;

---
+++ projetosjavas/aulas/src/aula/praticar/atividade.java
@@ -0,0 +1,13 @@
+public package aula.praticar;
+
+class atividade {
+
+    public static void main(String[] args) {//método construtor para rodar programa
+        String nome = "daniel cesar freitas"; //variavel para texto
+        byte idade = 17; //-128 até +128 variavel para numeros até 128 e -128
+
+        System.out.println(nome); //metodo para escrever nome na tela
+        System.out.println(idade); //metodo para escrevee idade na tela 
+        
+    }
+}
